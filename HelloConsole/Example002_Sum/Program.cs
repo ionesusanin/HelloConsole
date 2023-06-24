c3358153e138cc568da934fadae9cdd48bf985a0
ionesusanin <ionesusanin@yandex.ru>
2023-06-24 12:00:34 +0300

    problems

--- HelloConsole/Example002_Sum/Program.cs
+++ HelloConsole/Example002_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 3;
-int numberB = 2;
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+
+int numberB = new Random().Next(1,10);
 int result = numberA + numberB;
 Console.WriteLine(result);
